d6b0f4be37226d04fbbe319ca42da9399513c60b
priyasakshi <priyasakshi999@gmail.com>
2025-07-15 00:08:00 +0530

    Add FastAPI initialization and create hello world script

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from enum import Enum
 
+#Initialize FastAPI app
+
 app = FastAPI()
 
-# Pydantic models
+# Pydantic model
 class ItemBase(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
     description: Optional[str] = Field(None, max_length=300)
